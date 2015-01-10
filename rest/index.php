9307fc1c61f3361dfbd3d5b488d8ab4bb0dc2f8b
Colin <colin.calnan@gmail.com>
2015-01-09 23:14:39 -0800

    Moving REST stuff into 'rest' folder in prep for Angular app setup.

---
+++ rest/index.php
@@ -0,0 +1,46 @@
+<?php
+
+header('Content-type: application/json');
+
+require '../vendor/autoload.php';
+require_once 'config/db.php';
+
+Flight::route('GET /players', 'getPlayers');
+Flight::route('GET /players/@id', 'getPlayer');
+Flight::route('PUT /players/@id', 'updatePlayer');
+Flight::set('flight.log_errors', true);
+Flight::start();
+
+function getPlayers() {
+  try {
+    $database = getConnection();
+    $players = $database
+      ->select("players", "*", ["points[>]" => 0]);
+    Flight::json($players);
+  } catch(Exception $e) {
+    echo '{"error":{"text":'. $e->getMessage() .'}}';
+  }
+}
+
+function getPlayer($id) {
+  try {
+    $database = getConnection();
+    $player = $database
+      ->select("players", "*", ["id[=]" => $id]);
+    Flight::json($player);
+  } catch(Exception $e) {
+    echo '{"error":{"text":'. $e->getMessage() .'}}';
+  }
+}
+
+function updatePlayer() {
+  try {
+    $database = getConnection();
+    $id = Flight::request()->query['id'];
+    $player = $database
+      ->update("players", ["points[+]" => 5], ["id[=]" => $id]);
+  } catch(Exception $e) {
+    echo '{"error":{"text":'. $e->getMessage() .'}}';
+  }
+}
+?>
